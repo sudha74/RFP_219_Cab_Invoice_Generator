326cf43ae4862e3d7d8483a0e7f17c79c5c49ac5
sudha123 <sudhanaukala1997@gmail.com>
2022-12-21 20:06:41 +0530

    Added:Multiple Rides

--- src/main/java/com/bridgelabz/InvoiceService.java
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -1,18 +1,27 @@
 package com.bridgelabz;
 
+/**
+ * Purpose  - The Invoice generator should now take in multiple rides, and calculate the aggregate total for all
+ */
+
 public class InvoiceService {
-    /**
-     * Purpose  - Given distance and time, Invoice generator should return the total fare of the journey
-     */
     private static final double COST_PER_KM = 10;
     private static final double COST_PER_MIN = 1;
     private static final double MIN_FARE = 5.0;
 
     // Created a method to calculate the fare of journey
-    public double CalculateFare(double distance, int time) {
+    public double calculateFare(double distance, int time) {
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
         if (totalFare < MIN_FARE) {
             return MIN_FARE;
+        }
+        return totalFare;
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare = calculateFare(ride.getDistance(), ride.getTime());
         }
         return totalFare;
     }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -9,7 +9,7 @@ public class InvoiceServiceTest {
         InvoiceService invoiceService = new InvoiceService();
         double distance = 2.0;
         int time = 5;
-        double totalFare = invoiceService.CalculateFare(distance, time);
+        double totalFare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, totalFare, 0);
     }
 
@@ -18,7 +18,19 @@ public class InvoiceServiceTest {
         InvoiceService invoiceService = new InvoiceService();
         double distance = 0.1;
         int time = 1;
-        double totalFare = invoiceService.CalculateFare(distance, time);
+        double totalFare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(5.0, totalFare, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(5.0, 10),
+                new Ride(0.1, 1),
+                new Ride(20, 60)
+        };
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(rides);
+        Assert.assertEquals(260, totalFare, 0);
     }
 }
